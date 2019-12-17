a8f6ba184efe93299f3138e63114a534f7823b0f
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-16 23:50:18 -0500

    wrote main file skeleton

---
+++ SnackBarApp/src/snackbarApp/Main.java
@@ -0,0 +1,10 @@
+package snackbarApp;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Almost there");
+    }
+
+}
--- SnackBarApp/src/snackbarApp/Snack.java
+++ SnackBarApp/src/snackbarApp/Snack.java
@@ -4,17 +4,19 @@ public class Snack {
     private static float snackID = 0 * 0.850f;
     private float id;
     private String name;
+    private String button;
     private int quantity;
     private float cost;
     private float vendorCode = 0.0850f;
     private int machineId;
 
-    public Snack(String name, int quantity, float cost, int machineId) {
+    public Snack(String name, int quantity, float cost, int machineId, String button) {
         snackID++;
         id = snackID;
         this.name = name;
         this.quantity = quantity;
         this.cost = cost;
+        this.button = button ; // A3 B5 G2..etc
         this.machineId = machineId;
     }
 
@@ -57,11 +59,16 @@ public class Snack {
     }
     // set quantity (receives 1 arg, int)
     public void setQuantity(int quantity) {
-        this.quantity = quantity;
+        this.quantity += quantity;
     }
     // buy snack (receives 1 arg, int)
-    public int buySnack(int arg) {
-        
-    }
+    // public int buySnack(int snack-q) {
+
+    // }
     // get total cost given a quantity
+    public void getTotal(int quantity, String choice) {
+        // gonna need to search an object here.
+        float snack;
+        // choice = button number,
+    }
 }
