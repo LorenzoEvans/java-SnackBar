3c720ef95f45a3aa1e2bd29417a4eb672768b24d
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-22 19:23:46 -0500

    Now I can add an array of snacks

--- SnackBarApp/src/snackbarApp/Main.java
+++ SnackBarApp/src/snackbarApp/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Customer jane = new Customer("Jane", 45.25f);
         Customer bob = new Customer("Bob", 33.14f);
-        VendingMachine food = new VendingMachine("Food", {});
         Snack chips = new Snack("Chips", 36, 1.75f, "Food");
+        VendingMachine food = new VendingMachine("Food", chips);
         VendingMachine drink = new VendingMachine("Drink");
         VendingMachine office = new VendingMachine("Office");
         // food.setInventory({"Chips",});
